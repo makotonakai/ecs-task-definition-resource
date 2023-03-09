7a85d5b4f2ea6c693dc272666517f00753cf449a
MakotoNakai <macofandohr1812@gmail.com>
2023-03-09 13:58:56 +0900

    [feat] add struct for resource

---
+++ resource/resource.go
@@ -0,0 +1,14 @@
+package resource
+
+import (
+	"github.com/makotonakai/ecs-task-definition-resource/model"
+)
+type Source struct { 
+	AccessKeyId string `json:"aws_access_key_id"`
+	SecretAccessKey string `json:"aws_secret_access_key"`
+	Region string `json:"aws_region"`
+	TaskDefinitionJSON  model.TaskDefinision `json:"task_definition.json"`
+	Version model.Version `json:"version"`
+}
+
+
