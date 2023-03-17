fe75b1cf63b9250580c1e9ae67edb8360df1dbc5
MakotoNakai <macofandohr1812@gmail.com>
2023-03-17 12:10:32 +0900

    [feat] add params

---
+++ resource/params.go
@@ -0,0 +1,11 @@
+package resource
+
+import (
+	"github.com/makotonakai/ecs-task-definition-resource/model"
+)
+type Params struct { 
+	AccessKeyId string `json:"aws_access_key_id"`
+	SecretAccessKey string `json:"aws_secret_access_key"`
+	Region string `json:"aws_region"`
+	TaskDefinition model.TaskDefinision `json:"task_definition.json"`
+}
