7984dd04721fb92f76522892f4382c0649438375
MakotoNakai <macofandohr1812@gmail.com>
2023-03-17 16:53:52 +0900

    [fix] add versions

--- src/in/main.go
+++ src/in/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"encoding/json"
 	"github.com/makotonakai/ecs-task-definition-resource/resource"
 )
+
+type Request struct {
+	Version string `json:"version"`
+}
+
 
 type Response struct {
 	Version resource.Version `json:"version"`
@@ -12,10 +18,17 @@ type Response struct {
 
 func main() {
 
-	response := Response{}
+	var request Request
+	decoder := json.NewDecoder(os.Stdin)
+	err := decoder.Decode(&request)
+	if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to decode: %s\n", err.Error())
+			os.Exit(1)
+			return
+	}
 
-	version := resource.Version{Ref: "static"}
-	response.Version = version
+	response := Response{}
+	response.Version = resource.Version{Ref: request.Version}
 
 	json.NewEncoder(os.Stdout).Encode(response)
 	
