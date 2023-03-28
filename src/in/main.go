e393a7750a866022196652d711f839895ed3a161
MakotoNakai <macofandohr1812@gmail.com>
2023-03-28 09:33:51 +0900

    [fix] update input & output

--- src/in/main.go
+++ src/in/main.go
@@ -18,17 +18,17 @@ type Response struct {
 
 func main() {
 
-	// var request Request
-	// decoder := json.NewDecoder(os.Stdin)
-	// err := decoder.Decode(&request)
-	// if err != nil {
-	// 		fmt.Fprintf(os.Stderr, "failed to decode: %s\n", err.Error())
-	// 		os.Exit(1)
-	// 		return
-	// }
+	var request Request
+	decoder := json.NewDecoder(os.Stdin)
+	err := decoder.Decode(&request)
+	if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to decode: %s\n", err.Error())
+			os.Exit(1)
+			return
+	}
 
 	response := Response{}
-	response.Version = resource.Version{Ref: "static"}
+	response.Version = resource.Version{Ref: request.Version.Ref}
 
 	err := json.NewEncoder(os.Stdout).Encode(response)
 	if err != nil {
